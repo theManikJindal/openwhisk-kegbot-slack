22abcba4ef2b0cb28627dd67b41aee0e78bbc712
majindal <majindal@adobe.com>
2017-02-07 13:32:27 -0800

    initial flow integration

--- kegbot-slack-0.0.1.js
+++ kegbot-slack-0.0.1.js
@@ -9184,14 +9184,14 @@ RequestSigner.prototype.prepareRequest = function() {
       if (request.body && !headers['Content-Length'] && !headers['content-length'])
         headers['Content-Length'] = Buffer.byteLength(request.body)
 
-      if (this.credentials.sessionToken)
+      if (this.credentials.sessionToken && !headers['X-Amz-Security-Token'] && !headers['x-amz-security-token'])
         headers['X-Amz-Security-Token'] = this.credentials.sessionToken
 
-      if (this.service === 's3')
+      if (this.service === 's3' && !headers['X-Amz-Content-Sha256'] && !headers['x-amz-content-sha256'])
         headers['X-Amz-Content-Sha256'] = hash(this.request.body || '', 'hex')
 
-      if (headers['X-Amz-Date'])
-        this.datetime = headers['X-Amz-Date']
+      if (headers['X-Amz-Date'] || headers['x-amz-date'])
+        this.datetime = headers['X-Amz-Date'] || headers['x-amz-date']
       else
         headers['X-Amz-Date'] = this.getDateTime()
     }
@@ -9268,13 +9268,22 @@ RequestSigner.prototype.canonicalString = function() {
 
   var pathStr = this.parsedPath.path,
       query = this.parsedPath.query,
+      headers = this.request.headers,
       queryStr = '',
       normalizePath = this.service !== 's3',
       decodePath = this.service === 's3' || this.request.doNotEncodePath,
       decodeSlashesInPath = this.service === 's3',
       firstValOnly = this.service === 's3',
-      bodyHash = this.service === 's3' && this.request.signQuery ? 'UNSIGNED-PAYLOAD' :
-        (this.isCodeCommitGit ? '' : hash(this.request.body || '', 'hex'))
+      bodyHash
+
+  if (this.service === 's3' && this.request.signQuery) {
+    bodyHash = 'UNSIGNED-PAYLOAD'
+  } else if (this.isCodeCommitGit) {
+    bodyHash = ''
+  } else {
+    bodyHash = headers['X-Amz-Content-Sha256'] || headers['x-amz-content-sha256'] ||
+      hash(this.request.body || '', 'hex')
+  }
 
   if (query) {
     queryStr = encodeRfc3986(querystring.stringify(Object.keys(query).sort().reduce(function(obj, key) {
@@ -10213,7 +10222,7 @@ exports.getRoot = function getRoot (file) {
   }
 }
 
-}).call(this,"/Users/ddascal/Projects/openwhisk/openwhisk-kegbot-slack/node_modules/bindings/bindings.js")
+}).call(this,"/Users/majindal/workspace/br/openwhisk-kegbot-slack/node_modules/bindings/bindings.js")
 },{"fs":undefined,"path":undefined}],96:[function(require,module,exports){
 "use strict";
 module.exports = function(Promise) {
@@ -21819,7 +21828,7 @@ module.exports={
         "spec": ">=3.1.3 <3.2.0",
         "type": "range"
       },
-      "/Users/ddascal/Projects/openwhisk/openwhisk-kegbot-slack/node_modules/request"
+      "/Users/majindal/workspace/br/openwhisk-kegbot-slack/node_modules/request"
     ]
   ],
   "_from": "hawk@>=3.1.3 <3.2.0",
@@ -21849,7 +21858,7 @@ module.exports={
   "_shasum": "078444bd7c1640b0fe540d2c9b73d59678e8e1c4",
   "_shrinkwrap": null,
   "_spec": "hawk@~3.1.3",
-  "_where": "/Users/ddascal/Projects/openwhisk/openwhisk-kegbot-slack/node_modules/request",
+  "_where": "/Users/majindal/workspace/br/openwhisk-kegbot-slack/node_modules/request",
   "author": {
     "name": "Eran Hammer",
     "email": "eran@hammer.io",
@@ -64963,7 +64972,7 @@ module.exports={
         "spec": ">=2.3.0 <2.4.0",
         "type": "range"
       },
-      "/Users/ddascal/Projects/openwhisk/openwhisk-kegbot-slack/node_modules/request"
+      "/Users/majindal/workspace/br/openwhisk-kegbot-slack/node_modules/request"
     ]
   ],
   "_from": "tough-cookie@>=2.3.0 <2.4.0",
@@ -64997,7 +65006,7 @@ module.exports={
   "_shasum": "f081f76e4c85720e6c37a5faced737150d84072a",
   "_shrinkwrap": null,
   "_spec": "tough-cookie@~2.3.0",
-  "_where": "/Users/ddascal/Projects/openwhisk/openwhisk-kegbot-slack/node_modules/request",
+  "_where": "/Users/majindal/workspace/br/openwhisk-kegbot-slack/node_modules/request",
   "author": {
     "name": "Jeremy Stashewsky",
     "email": "jstashewsky@salesforce.com"
@@ -72666,7 +72675,7 @@ module.exports={
         "spec": ">=2.1.1 <3.0.0",
         "type": "range"
       },
-      "/Users/ddascal/Projects/openwhisk/openwhisk-kegbot-slack/node_modules/@slack/client"
+      "/Users/majindal/workspace/br/openwhisk-kegbot-slack/node_modules/@slack/client"
     ]
   ],
   "_from": "winston@>=2.1.1 <3.0.0",
@@ -72700,7 +72709,7 @@ module.exports={
   "_shasum": "0b48420d978c01804cf0230b648861598225a119",
   "_shrinkwrap": null,
   "_spec": "winston@^2.1.1",
-  "_where": "/Users/ddascal/Projects/openwhisk/openwhisk-kegbot-slack/node_modules/@slack/client",
+  "_where": "/Users/majindal/workspace/br/openwhisk-kegbot-slack/node_modules/@slack/client",
   "author": {
     "name": "Charlie Robbins",
     "email": "charlie.robbins@gmail.com"
@@ -76437,176 +76446,23 @@ function extend() {
 "use strict";Object.defineProperty(exports,"__esModule",{value:!0});/**
  * The entry point for the action.
  * @param params Input object
- * "data": {
-                    "drink": {
-                        "duration": 8,
-                        "id": 25,
-                        "images": [
-                            {
-                                "keg_id": 1,
-                                "original_url": "http://52.204.245.247:80/media/pics/20170207020706-0dd12e706991423ab3f9282aef2143a8.jpg",
-                                "session_id": 2,
-                                "thumbnail_url": "http://52.204.245.247:80/media/CACHE/images/pics/20170207020706-0dd12e706991423ab3f9282aef2143a8/b7fddff42dc5c48e9ab6acf30db5f4a4.jpg",
-                                "time": "2017-02-07T02:07:06.706214+00:00",
-                                "url": "http://52.204.245.247:80/media/CACHE/images/pics/20170207020706-0dd12e706991423ab3f9282aef2143a8/9635cd5de7ced77f3bb20097b16ba25f.jpg",
-                                "user_id": "guest"
-                            }
-                        ],
-                        "keg": {
-                            "beverage": {
-                                "beverage_type": "beer",
-                                "color_hex": "#C35900",
-                                "id": 1,
-                                "name": "Racer 5",
-                                "producer": {
-                                    "country": "USA",
-                                    "description": "",
-                                    "id": 1,
-                                    "is_homebrew": false,
-                                    "name": "Bear Republic",
-                                    "origin_city": "",
-                                    "origin_state": "",
-                                    "url": ""
-                                },
-                                "style": "IPA"
-                            },
-                            "end_time": "2017-02-04T02:19:41+00:00",
-                            "full_volume_ml": 18927.1,
-                            "id": 1,
-                            "illustration_thumbnail_url": "http://52.204.245.247:80/static/images/keg/thumb/keg-srm14-3.png",
-                            "illustration_url": "http://52.204.245.247:80/static/images/keg/full/keg-srm14-3.png",
-                            "keg_type": "corny",
-                            "online": true,
-                            "percent_full": 85.56519007618225,
-                            "remaining_volume_ml": 16195.009090909089,
-                            "served_volume_ml": 2732.09090909091,
-                            "size": {
-                                "id": 0,
-                                "name": "corny",
-                                "volume_ml": 18927.1
-                            },
-                            "size_id": 0,
-                            "size_name": "corny",
-                            "size_volume_ml": 18927.1,
-                            "spilled_ml": 0,
-                            "spilled_volume_ml": 0,
-                            "start_time": "2017-02-04T02:19:41+00:00",
-                            "type": {
-                                "abv": 0,
-                                "brewer_id": "1",
-                                "id": "1",
-                                "name": "Racer 5",
-                                "style_id": "0"
-                            },
-                            "type_id": "1",
-                            "volume_ml_remain": 16195.009090909089
-                        },
-                        "keg_id": 1,
-                        "session": {
-                            "end_time": "2017-02-07T05:07:06.701949+00:00",
-                            "id": 2,
-                            "name": "",
-                            "start_time": "2017-02-07T02:02:06+00:00",
-                            "url": "/sessions/2017/2/6/2",
-                            "volume_ml": 1888
-                        },
-                        "session_id": 2,
-                        "tick_time_series": "0:1 304:6 1946:36 2116:5 2297:4 2505:5 2709:5 2906:2 3120:4 3302:3 3492:1 3694:1 3902:1 8284:0",
-                        "ticks": 74,
-                        "time": "2017-02-07T02:07:06.701949+00:00",
-                        "url": "/drinks/25",
-                        "user": {
-                            "display_name": "guest",
-                            "is_active": true,
-                            "url": "/drinkers/guest",
-                            "username": "guest"
-                        },
-                        "user_id": "guest",
-                        "volume_ml": 224
-                    },
-                    "drink_id": 25,
-                    "id": 31,
-                    "keg": {
-                        "beverage": {
-                            "beverage_type": "beer",
-                            "color_hex": "#C35900",
-                            "id": 1,
-                            "name": "Racer 5",
-                            "producer": {
-                                "country": "USA",
-                                "description": "",
-                                "id": 1,
-                                "is_homebrew": false,
-                                "name": "Bear Republic",
-                                "origin_city": "",
-                                "origin_state": "",
-                                "url": ""
-                            },
-                            "style": "IPA"
-                        },
-                        "end_time": "2017-02-04T02:19:41+00:00",
-                        "full_volume_ml": 18927.1,
-                        "id": 1,
-                        "illustration_thumbnail_url": "http://52.204.245.247:80/static/images/keg/thumb/keg-srm14-3.png",
-                        "illustration_url": "http://52.204.245.247:80/static/images/keg/full/keg-srm14-3.png",
-                        "keg_type": "corny",
-                        "online": true,
-                        "percent_full": 85.56519007618225,
-                        "remaining_volume_ml": 16195.009090909089,
-                        "served_volume_ml": 2732.09090909091,
-                        "size": {
-                            "id": 0,
-                            "name": "corny",
-                            "volume_ml": 18927.1
-                        },
-                        "size_id": 0,
-                        "size_name": "corny",
-                        "size_volume_ml": 18927.1,
-                        "spilled_ml": 0,
-                        "spilled_volume_ml": 0,
-                        "start_time": "2017-02-04T02:19:41+00:00",
-                        "type": {
-                            "abv": 0,
-                            "brewer_id": "1",
-                            "id": "1",
-                            "name": "Racer 5",
-                            "style_id": "0"
-                        },
-                        "type_id": "1",
-                        "volume_ml_remain": 16195.009090909089
-                    },
-                    "keg_id": 1,
-                    "kind": "drink_poured",
-                    "session": {
-                        "end_time": "2017-02-07T05:07:06.701949+00:00",
-                        "id": 2,
-                        "is_active": true,
-                        "name": "",
-                        "start_time": "2017-02-07T02:02:06+00:00",
-                        "url": "/sessions/2017/2/6/2",
-                        "volume_ml": 1888
-                    },
-                    "session_id": 2,
-                    "time": "2017-02-07T02:07:06.701949+00:00",
-                    "user": {
-                        "date_joined": "2017-02-04T02:13:24+00:00",
-                        "display_name": "guest",
-                        "email": "",
-                        "is_active": true,
-                        "is_staff": false,
-                        "is_superuser": false,
-                        "last_login": "2017-02-04T02:13:24+00:00",
-                        "url": "/drinkers/guest",
-                        "username": "guest"
-                    },
-                    "user_id": "guest"
-                },
- "param1": "https://hooks.slack.com/services/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",
- "type": "event"
- }
+ * 
  * @returns {Promise}
  */function main(a){return new Promise(function(b,c){//send message to slack channel
-try{var d=require("@slack/client").IncomingWebhook}catch(k){console.log(k),c({message:"Could not load @slack/client",error:k.toString()})}var e=a.slack_webhook_url,f=new d(e);console.log(a);var g=Math.round(100*(0.033814*a.data.drink.volume_ml))/100,h=a.data.drink.user.display_name+" poured "+g+"oz of "+a.data.drink.keg.beverage.name,i=a.data.drink.images,j={attachments:[{fallback:h,color:"#36a64f",pretext:"",text:h,image_url:null!==i&&"undefined"!=typeof i?i[0].original_url:a.data.drink.keg.illustration_thumbnail_url,thumb_url:null!==i&&"undefined"!=typeof i?i[0].thumbnail_url:a.data.drink.keg.illustration_thumbnail_url,footer:"Kegbot API",footer_icon:"https://platform.slack-edge.com/img/default_application_icon.png"}]};f.send(j,function(k,l){k?(console.log("Error:",k),c(a)):(console.log("Message sent: ",l),b(a))})})}exports.default=main;
+try{var d=require("@slack/client").IncomingWebhook}catch(h){console.log(h),c({message:"Could not load @slack/client",error:h.toString()})}var e=a.slack_webhook_url,f=new d(e);console.log(a);var g=a.asset.name+" created.";/*var slack_message = {
+                attachments: [
+                    {
+                        fallback: slack_message_text,
+                        color: "#36a64f",
+                        pretext: "",
+                        text: slack_message_text,
+                        "image_url": images !== null && typeof(images) !== "undefined" ? images[0].original_url : params.data.drink.keg.illustration_thumbnail_url,
+                        "thumb_url": images !== null && typeof(images) !== "undefined" ? images[0].thumbnail_url : params.data.drink.keg.illustration_thumbnail_url,
+                        "footer": "Kegbot API",
+                        "footer_icon": "https://platform.slack-edge.com/img/default_application_icon.png"
+                    }
+                ]
+            }*/f.send(g,function(h,i){h?(console.log("Error:",h),c(a)):(console.log("Message sent: ",i),b(a))})})}exports.default=main;
 
 },{"@slack/client":1}]},{},[]);
 var main = require('main-action').default;
